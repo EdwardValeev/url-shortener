1aa3f7401915a2e70a1b9e8be00047a6050563c2
EdwardValeev <edwardvaleevev@gmail.com>
2025-03-26 15:14:52 +0500

    USH#121 Поправил работу с postgres'ом

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -2,11 +2,13 @@ package postgresql
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"time"
 	"url-shortener/internal/storage"
 )
 
@@ -17,17 +19,24 @@ type Storage struct {
 func New(connStr string) (*Storage, error) {
 	const op = "storage.postgresql.New"
 
-	db, err := pgxpool.New(context.Background(), connStr)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, connStr)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	err = db.Ping(context.Background())
-	if err != nil {
+	// Проверяем соединение с БД
+	if err := db.Ping(ctx); err != nil {
+		db.Close()
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	_, err = db.Exec(context.Background(), `
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = db.Exec(ctx, `
 	CREATE TABLE IF NOT EXISTS url(
 	    id SERIAL PRIMARY KEY,
 	    alias TEXT NOT NULL UNIQUE,
@@ -35,23 +44,34 @@ func New(connStr string) (*Storage, error) {
 	CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
 	`)
 	if err != nil {
+		db.Close()
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return &Storage{db: db}, nil
 }
 
-func (s *Storage) SaveURL(ctx context.Context, urlToSave, alias string) (int64, error) {
+func (s *Storage) SaveURL(urlToSave, alias string) (int64, error) {
 	const op = "storage.postgresql.SaveURL"
 	var id int64
 
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	err := s.db.QueryRow(ctx, `
-		INSERT INTO url(url, alias) VALUES($1, $2) RETURNING id;
+		INSERT INTO url(url, alias) 
+		VALUES($1, $2) 
+		RETURNING id;
 	`, urlToSave, alias).Scan(&id)
 	if err != nil {
 		// Обработка ошибки уникальности (если alias уже существует)
-		if pgErr, ok := err.(*pgconn.PgError); ok && pgErr.Code == "23505" { // Код 23505 = violation of unique constraint
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
 			return 0, fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, fmt.Errorf("%s: no rows returned: %w", op, err)
 		}
 
 		// Обработка других ошибок
@@ -61,14 +81,17 @@ func (s *Storage) SaveURL(ctx context.Context, urlToSave, alias string) (int64, 
 	return id, nil
 }
 
-func (s *Storage) GetURL(ctx context.Context, alias string) (string, error) {
+func (s *Storage) GetURL(alias string) (string, error) {
 	const op = "storage.postgresql.GetURL"
 	var url string
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	err := s.db.QueryRow(ctx, `SELECT url FROM url WHERE alias = $1;`, alias).Scan(&url)
 	if err != nil {
 		// Обработка ошибки отсутствия строки по ключу (запись не найдена)
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return "", fmt.Errorf("%s: %w", op, storage.ErrURLNotFound)
 		}
 		// Обработка других ошибок
@@ -78,8 +101,11 @@ func (s *Storage) GetURL(ctx context.Context, alias string) (string, error) {
 	return url, nil
 }
 
-func (s *Storage) DeleteURL(ctx context.Context, alias string) error {
+func (s *Storage) DeleteURL(alias string) error {
 	const op = "storage.postgresql.DeleteURL"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	result, err := s.db.Exec(ctx, `DELETE FROM url WHERE alias = $1`, alias)
 	if err != nil {
@@ -87,8 +113,7 @@ func (s *Storage) DeleteURL(ctx context.Context, alias string) error {
 	}
 
 	// Проверяем, сколько строк было удалено
-	rowsAffected := result.RowsAffected()
-	if rowsAffected == 0 {
+	if rowsAffected := result.RowsAffected(); rowsAffected == 0 {
 		// Если ни одна строка не была удалена, возвращаем ошибку
 		return fmt.Errorf("%s: %w", op, storage.ErrURLNotFound)
 	}
